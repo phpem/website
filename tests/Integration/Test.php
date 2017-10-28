a419438c956d99ff5fce3d131c5d5387e186205f
Matt Brunt <matt@mfyu.co.uk>
2017-10-28 16:11:52 +0100

    Add tests

---
+++ tests/Integration/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /** @test */
+    public function it_does_a_thing()
+    {
+        self::assertTrue(true);
+    }
+}
